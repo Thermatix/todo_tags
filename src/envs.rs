b79326f8bf85866aeb5440274c2eba29ad2ebb80
Martin Becker <mbeckerwork@gmail.com>
2021-10-11 23:31:37 +0100

    Makes TDT_ prefix implicit for ENVS

--- src/envs.rs
+++ src/envs.rs
@@ -6,7 +6,7 @@ use dirs;
 macro_rules! declare_env {
     ($env:ident: $env_var:ident, $def:expr, $type:ty) => {
         pub fn $env() -> $type {
-            match env::var("$env_var") {
+            match env::var("TDT_$env_var") {
                 Ok(res) => res.into(),
                 _ => $def.into()
             }
@@ -14,21 +14,22 @@ macro_rules! declare_env {
     };
     ($env:ident: $env_var:ident, $def:expr) => {
         pub fn $env() -> String {
-            match env::var("$env_var") {
-                Ok(res) => res.into(),
-                _ => $def.into()
+            match env::var("TDT_$env_var") {
+                Ok(res) => res,
+                _ => $def.to_string()
             }
         }
     }
 }
 
-declare_env!(server_addr: TDT_SERVER_ADDRESS, "[::1]:50051");
-declare_env!(std_out: TDT_SERVER_STDOUT, "/tmp/tdt.out");
-declare_env!(std_err: TDT_SERVER_STDERR, "/tmp/tdt.err");
-declare_env!(pid_file: TDT_PID_FILE, "/tmp/tdt.pid");
-declare_env!(working_dir: TDT_WORKING_DIR, dirs::home_dir().unwrap().to_str().unwrap());
-declare_env!(chown_pid: TDT_CHOWN_PID, "false");
-declare_env!(user: TDT_USER, "nobody");
-declare_env!(group: TDT_GROUP, "nobody");
-declare_env!(umask: TDT_UMASK, "777");
+declare_env!(server_addr: SERVER_ADDRESS, "[::1]:50051");
+declare_env!(std_out: SERVER_STDOUT, "/tmp/tdt.out");
+declare_env!(std_err: SERVER_STDERR, "/tmp/tdt.err");
+declare_env!(pid_file: PID_FILE, "/tmp/tdt.pid");
+declare_env!(working_dir: WORKING_DIR, dirs::home_dir().unwrap().to_str().unwrap());
+declare_env!(chown_pid: CHOWN_PID, "false");
+declare_env!(user: USER, "nobody");
+declare_env!(group: GROUP, "nobody");
+declare_env!(umask: UMASK, "777");
+
 
