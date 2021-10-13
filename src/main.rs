6368421ec98c41e320b68ad3e9755b33f29bb1ad
Martin Becker <mbeckerwork@gmail.com>
2021-10-13 20:35:34 +0100

    Move arg parsing to main

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ mod macros;
 mod gprc;
 mod server;
 mod client;
-mod app;
 
 mod envs;
+
+use clap::{Arg, App};
 use color_eyre::{self, Report};
 use tracing::info;
 use tracing_subscriber::{self, EnvFilter};
@@ -13,11 +14,45 @@ use tracing_subscriber::{self, EnvFilter};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>  {
     setup()?;
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+                        .about(env!("CARGO_PKG_DESCRIPTION"))
+                        .author(env!("CARGO_PKG_AUTHORS"))
+                        .version(env!("CARGO_PKG_VERSION"))
+                        .usage("tdt [command]")
+                        .subcommand(
+                            App::new("add")
+                            .about("Add a project folder")
+                            .alias("a")
+                            .usage("tdt add(a) [project/folder] (name)")
+                        ).subcommand(
+                            App::new("edit")
+                            .about("Edit project meta-data")
+                            .alias("e")
+                            .usage("tdt edit(e) [project name]")
+                        ).subcommand(
+                            App::new("display")
+                            .about("Display the project todo list")
+                            .alias("d")
+                            .usage("tdt display(d) [project name]")
+                        ).subcommand(
+                            App::new("write")
+                            .about("Write a given item")
+                            .alias("w")
+                            .usage("tdt write(w) [item] [about]")
+                        ).subcommand(
+                            App::new("remove")
+                            .about("Remove the given item")
+                            .alias("r")
+                            .usage("tdt remove(r) [item]")
+                        ).subcommand(
+                            App::new("show")
+                            .about("Show the given item")
+                            .alias("s")
+                            .usage("tdt show [item]")
+                        ).get_matches();
 
-    let app = app::new();
     let mut server = server::Server::new();
     server.start().await;
-    app.run(envs::args().collect());
     Ok(())
 }
 
@@ -37,3 +72,22 @@ fn setup() -> Result<(), Report> {
     Ok(())
 }
 
+async fn add_action(c: &Context) {
+    let mut client = Client::new().await; 
+    let  folder_name =
+    match (c.args.get(0), c.args.get(1)) {
+        (Some(folder), Some(name)) => (folder.clone(), name.clone()),
+        (Some(folder), None) => (folder.clone(), folder.clone()),
+        _ => panic!("No folder or folder & project name provided")
+    };
+    match client.request(Action::Add(folder_name.into())).await {
+        Ok(r) => {
+            let response: data_types::Response = r.into();
+        }
+    };
+}
+fn edit_action(c: &Context) {}
+fn display_action(c: &Context) {}
+fn write_action(c: &Context) {}
+fn remove_action(c: &Context) {}
+fn show_action(c: &Context) {}
