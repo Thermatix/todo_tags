168653e6b0fe1fa21117b4a8c03966ddfcd690be
Martin Becker <mbeckerwork@gmail.com>
2021-10-13 22:41:20 +0100

    Add From<((cmd, ArgsMatcher))> for Action

--- src/gprc.rs
+++ src/gprc.rs
@@ -1,5 +1,7 @@
 tonic::include_proto!("todo_tags");
 
+use super::clap::ArgMatches;
+
 pub mod data_types {
     pub use super::{Item, Items, Query, Project,  ResType, Response};
 }
@@ -13,13 +15,77 @@ pub enum Action {
     Remove(Item),
     Show(Item),
     Find(Query),
-}   
+}
+
+impl<'a> From<(&'a str, Option<&ArgMatches<'a >>)> for Action {
+    fn from((cmd, args): (&'a str, Option<&ArgMatches<'a>>)) -> Self {
+        if let Some(args) = args {
+            match cmd {
+                "add" => {
+                    Self::Add(
+                        (
+                            args.value_of("FOLDER").unwrap().to_owned(),
+                            args.value_of("NAME").unwrap_or(args.value_of("FOLDER").unwrap()).to_owned()
+                        ).into()
+                        )
+                },               "edit" => {
+                    Self::Edit(
+                        (
+                            args.value_of("FOLDER").unwrap().to_owned(),
+                            args.value_of("NAME").unwrap_or(args.value_of("FOLDER").unwrap()).to_owned()
+                        ).into()
+                        )
+                },
+                "display" => {
+                    Self::Display(
+                        (
+                            args.value_of("FOLDER").unwrap().to_owned(),
+                            args.value_of("NAME").unwrap_or(args.value_of("FOLDER").unwrap()).to_owned()
+                        ).into()
+                        )
+                },
+                "write" => {
+                    Self::Write(
+                        (
+                            args.value_of("NAME").unwrap().to_owned(),
+                            args.value_of("description").unwrap().to_owned()
+
+                        ).into()
+                        )
+                },               "remove" => {
+                    Self::Remove(
+                        (
+                            args.value_of("NAME").unwrap().to_owned(),
+                            "".to_owned()
+
+                        ).into()
+                        )
+                },               "show" => {
+                    Self::Show(
+                        (
+                            args.value_of("NAME").unwrap().to_owned(),
+                            "".to_owned()
+
+                        ).into()
+                        )
+                },               "find" => {
+                    Self::Find(
+                        args.value_of("QUERY").unwrap().to_owned().into()
+                        )
+                },
+                _ => { panic!("Unknown action, given {}", cmd) }
+            }
+        } else {
+            panic!("No args found!")
+        }
+    }
+}
 
 
 pub mod primatives {
     pub trait Conversion {}
 
-    pub type Item = (String, String, Vec<String>);
+    pub type Item = (String, String);
     pub type ItemsWithCount = (u64, Vec<Item>);
     pub type ItemsWithOutCount = Vec<Item>;
     pub type FolderName = (String, String);
@@ -33,7 +99,7 @@ pub mod primatives {
             }
         }
     }
-    
+
     impl Conversion for super::Query {}
     impl Conversion for super::Project {}
     impl Conversion for super::Item {}
@@ -95,9 +161,9 @@ pub mod primatives {
     impl From<Item> for super::Item {
         fn from(v: Item) -> Self {
             Self {
-               tag: v.0,
-               description: v.1,
-               file_path: v.2,
+                tag: v.0,
+                description: v.1,
+                file_paths: Vec::new(),
             }
         }
     }
@@ -105,9 +171,9 @@ pub mod primatives {
     impl From<&Item> for super::Item {
         fn from(v: &Item) -> Self {
             Self {
-               tag: v.0.clone(),
-               description: v.1.clone(),
-               file_path: v.2.clone(),
+                tag: v.0.clone(),
+                description: v.1.clone(),
+                file_paths: Vec::new(),
             }
         }
     }
