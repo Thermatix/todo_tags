372e01656d03946ba684eae9a2d4d64fb472d7e9
Martin Becker <mbeckerwork@gmail.com>
2021-11-05 21:56:35 +0000

    Change build script to not use tonic build macro

--- build.rs
+++ build.rs
@@ -1,5 +1,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/todo_tags.proto")?;
+    let proto_path = "proto".to_string();
+    let files = &["todo_tags"];
+
+    tonic_build::configure()
+    // .type_attribute(".todoTags.Item", "use serde_derive::{Serialize, Deserialize};")
+    // .type_attribute(".", "#[derive(Serialize, Deserialize)]")
+    .compile(
+            files.iter()
+                  .map(|file| format!("{}/{}.proto", &proto_path, file) )
+                  .collect::<Vec<String>>().as_ref(),
+            &[proto_path]
+        )?;
     Ok(())
 }
 
